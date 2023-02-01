d38ed4f743e37e05d485fbe0a09da1597221b228
Ulenson <prosaic-abroad-0f@icloud.com>
2023-02-02 02:19:58 +0300

    урок

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:base/screens/login_screen.dart';
+import 'package:base/screens/profile_screen.dart';
+import 'package:base/screens/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (_) => LoginScreen(),
+        '/sign_up': (_) => SignUpScreen(),
+        '/profile': (_) => const ProfileScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../service/fire_base_helper.dart';
+class ProfileScreen extends StatefulWidget {
+
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  var username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initUsername();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              FirebaseHelper.logout();
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+            child: const Text(
+              'Logout',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('Hello, $username!')),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _initUsername() async {
+    final email = FirebaseAuth.instance.currentUser?.email ?? '';
+    setState(() {
+      username = email;
+    });
+  }
+}
