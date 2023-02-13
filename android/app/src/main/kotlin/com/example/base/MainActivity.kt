4cf386eaf97172db7d8dc676306d94c358f6c06a
Ulenson <prosaic-abroad-0f@icloud.com>
2023-02-13 22:37:37 +0300

    firebase deploy done

--- android/app/src/main/kotlin/com/example/base/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/base/MainActivity.kt
@@ -1,6 +1,3 @@
 package com.example.base
-
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+import io.flutter.embedding.android.FlutterFragmentActivity
--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,16 @@ import 'package:base/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:base/screens/forgot_password_screen.dart';
+
+
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = "pk_test_51Mb4UeL4wGVBAypjKi9uCkgmxwTWItU2U4NIBs0JDoGO4G6CG0AKtdWp2c7ZHIPvPSKe0WkomT3DSChNqhl1nTlb00g452MGDS";
+  await Stripe.instance.applySettings();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -30,7 +37,7 @@ class MyApp extends StatelessWidget {
         '/login': (_) => LoginScreen(),
         '/sign_up': (_) => SignUpScreen(),
         '/profile': (_) => const ProfileScreen(),
-        '/forgot': (_) => ForgotPass(),
+        '/forgot': (_) => const ForgotPass(),
       },
     );
   }
