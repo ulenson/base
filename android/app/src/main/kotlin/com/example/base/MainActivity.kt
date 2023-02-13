3150141f6c6b1f1a4d434c250353fdb1a6ad77a2
Ulenson <prosaic-abroad-0f@icloud.com>
2023-02-13 22:38:42 +0300

    firebase deploy done

--- android/app/src/main/kotlin/com/example/base/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/base/MainActivity.kt
@@ -1,3 +1,5 @@
 package com.example.base
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+}
